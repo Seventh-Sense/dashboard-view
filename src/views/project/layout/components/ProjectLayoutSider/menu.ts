5f3f870698a5642e9eb83ac40060a292c5e0b189
Seventh-Sense <1577257077@qq.com>
2024-12-13 10:17:20 +0800

    feat: annotation Bacnet

--- src/views/project/layout/components/ProjectLayoutSider/menu.ts
+++ src/views/project/layout/components/ProjectLayoutSider/menu.ts
@@ -19,20 +19,20 @@ export const menuOptionsInit = () => {
   const t = window['$t']
 
   return reactive([
-    {
-      label: () =>
-        h(
-          RouterLink,
-          {
-            to: {
-              name: PageEnum.BASE_LOCAL_MAN_NAME
-            }
-          },
-          { default: () => 'Bacnet' }
-        ),
-      key: PageEnum.BASE_LOCAL_MAN_NAME,
-      icon: renderImage(SVG_ICON.card_icons.Bacnet_nomal, '', 32, 32),
-    },
+    // {
+    //   label: () =>
+    //     h(
+    //       RouterLink,
+    //       {
+    //         to: {
+    //           name: PageEnum.BASE_LOCAL_MAN_NAME
+    //         }
+    //       },
+    //       { default: () => 'Bacnet' }
+    //     ),
+    //   key: PageEnum.BASE_LOCAL_MAN_NAME,
+    //   icon: renderImage(SVG_ICON.card_icons.Bacnet_nomal, '', 32, 32),
+    // },
     {
       label: () =>
         h(
